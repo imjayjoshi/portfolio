701a7be4522c7728196d1c188f3be06eab036d20
jaybudventure <jay.budventure@gmail.com>
2026-05-06 10:43:02 +0530

    stop devtools

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TransitionProvider } from "@/components/transitions";
-import { DisableDevTools } from "@/components/ui/DisableDevTools";
+// import { DisableDevTools } from "@/components/ui/DisableDevTools";
 
 const queryClient = new QueryClient();
 
@@ -18,7 +18,7 @@ export function Providers({ children }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <DisableDevTools />
+        {/* <DisableDevTools /> */}
         <TransitionProvider>{children}</TransitionProvider>
         <Toaster />
         <Sonner />
